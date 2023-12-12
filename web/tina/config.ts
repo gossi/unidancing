22864ff1d9183503c66798b403f59e6600e68174
gossi <gossi@users.noreply.github.com>
2023-12-12 17:53:02 +0100

    special treatment for prod build

--- web/tina/config.ts
+++ web/tina/config.ts
@@ -4,15 +4,20 @@ import process from 'node:process';
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.CF_PAGES_BRANCH || 'main';
+const prod = process.env.PROD ?? false;
 
 export default defineConfig({
   branch,
   clientId: process.env.TINA_CLIENT_ID,
   token: process.env.TINA_TOKEN,
 
+  // build: {
+  //   outputFolder: "admin",
+  //   publicFolder: "public",
+  // },
   build: {
     outputFolder: "admin",
-    publicFolder: "public",
+    publicFolder: prod ? "dist" : 'public',
   },
   media: {
     tina: {
