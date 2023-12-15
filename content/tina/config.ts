446521d6f6fec554f02eb296f06a62dfd90f869d
gossi <gossi@users.noreply.github.com>
2023-12-15 14:23:16 +0100

    custom config for "content repo"

--- content/tina/config.ts
+++ content/tina/config.ts
@@ -1,1 +1,14 @@
-export * from '../../web/tina/config';
+import config from '../../web/tina/config';
+
+const configWithCustomization = {
+  ...config,
+  media: {
+    ...config.media,
+    tina: {
+      ...config.media?.tina,
+      publicFolder: 'web/public'
+    }
+  }
+};
+
+export default configWithCustomization;
