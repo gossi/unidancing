e6bcfaba952078382927d5bfed4d095c8aa0e33a
gossi <gossi@users.noreply.github.com>
2023-12-23 23:06:54 +0100

    remove legacy database

--- web/app/database/utils.ts
+++
@@ -1,7 +0,0 @@
-export function ensureArray<T>(stringOrArray?: T | T[]): T[] {
-  return stringOrArray
-    ? Array.isArray(stringOrArray)
-      ? stringOrArray
-      : [stringOrArray]
-    : [];
-}
