f0c7578d9c6d34cac314572993482f28fa7f5c90
gossi <gossi@users.noreply.github.com>
2024-10-25 18:16:10 +0200

    update deps

--- web/types/global.d.ts
+++ web/types/global.d.ts
@@ -1,6 +1,8 @@
 // Types for compiled templates
 declare module '@unidancing/app/templates/*' {
   import type { TemplateFactory } from 'htmlbars-inline-precompile';
+
   const tmpl: TemplateFactory;
+
   export default tmpl;
 }
