253734753d94f8b09b76f0df01589faaf414c504
Thomas Gossmann <gossi@users.noreply.github.com>
2024-10-27 15:21:45 +0100

    Bring back Spotify player selection (#63)

--- web/app/domain/supporting/spotify/client.ts
+++ web/app/domain/supporting/spotify/client.ts
@@ -1,31 +1,39 @@
 import { tracked } from '@glimmer/tracking';
 
+import { dropTask } from 'ember-concurrency';
 import { useMachine } from 'ember-statecharts';
 import SpotifyWebApi from 'spotify-web-api-js';
 import SpotifyPlayer from 'spotify-web-playback';
 
 import { SpotifyMachine } from './machine';
 
-import type { Track } from './domain-objects';
+import type { Device, Track } from './domain-objects';
 
 export class SpotifyClient {
+  @tracked devices: Device[] = [];
+  @tracked device?: Device;
   @tracked error?: string;
   @tracked track?: Track;
 
   api = new SpotifyWebApi();
   player = new SpotifyPlayer('UniDancing');
 
-  private deviceId?: string = undefined;
+  // private deviceId?: string = undefined;
 
   statechart = useMachine(this, () => {
-    const { connectPlayer, loadPlayback } = this;
+    const { loadDevices, autoselectDevice, connectPlayer, loadPlayback, hasDevices } = this;
 
     return {
       machine: SpotifyMachine.withConfig({
+        guards: {
+          hasDevices
+        },
         services: {
           connectPlayer: async (_, { accessToken }) => {
             await connectPlayer(accessToken as string);
           },
+          loadDevices: () => loadDevices.perform(),
+          autoselectDevice,
           loadPlayback
         }
       }),
@@ -35,16 +43,43 @@ export class SpotifyClient {
     };
   });
 
-  loadPlayback = () => async () => {
-    const playing = await this.player.getPlaybackState();
+  // AUTH
 
-    if (playing?.item) {
-      this.track = playing.item as unknown as Track;
-    }
+  get authenticated() {
+    return this.statechart.state?.matches('authenticated');
+  }
 
-    if (playing?.is_playing) {
-      this.statechart.send('play');
+  authenticate = async (accessToken: string) => {
+    try {
+      this.api.setAccessToken(accessToken);
+
+      await this.api.getMe();
+
+      this.statechart.send('authenticate', { accessToken });
+
+      return true;
+    } catch {
+      this.api.setAccessToken(null);
+
+      return false;
     }
+  };
+
+  // DEVICES
+
+  get ready() {
+    return (
+      this.statechart.state?.matches({
+        authenticated: 'ready'
+      }) ||
+      this.statechart.state?.matches({
+        authenticated: 'playback'
+      })
+    );
+  }
+
+  hasDevices = () => {
+    return this.player.ready || this.devices.length > 0;
   };
 
   connectPlayer = async (accessToken: string) => {
@@ -68,19 +103,54 @@ export class SpotifyClient {
 
     if (!this.player.ready) {
       throw new Error();
-    } else {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      this.deviceId = this.player._deviceId;
     }
   };
 
-  // exported states
+  loadDevices = dropTask(async () => {
+    this.devices = (await this.api.getMyDevices()).devices;
+  });
 
-  get authenticated() {
-    return this.statechart.state?.matches('authenticated');
-  }
+  autoselectDevice = async () => {
+    if (this.player.ready) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      await this.selectDevice({
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        id: this.player._deviceId,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        name: this.player._name,
+        type: 'Computer'
+      });
+    }
+
+    const device =
+      // at first try to find the active device
+      (this.devices.find((dev) => dev.is_active) ??
+      // if not found, see if there is only one, then pick that one
+      this.devices.length === 1)
+        ? this.devices[0]
+        : undefined;
+
+    if (device) {
+      await this.selectDevice(device);
+    }
+  };
+
+  selectDevice = async (device: Device) => {
+    try {
+      await this.api.transferMyPlayback([device.id as string]);
+      this.device = device;
+      this.statechart.send('selectDevice');
+      // eslint-disable-next-line no-empty
+    } catch {}
+  };
+
+  // PLAYBACK
 
   get playing() {
     return this.statechart.state?.matches({
@@ -90,39 +160,20 @@ export class SpotifyClient {
     });
   }
 
-  get ready() {
-    return (
-      this.statechart.state?.matches({
-        authenticated: 'ready'
-      }) ||
-      this.statechart.state?.matches({
-        authenticated: 'playback'
-      })
-    );
-  }
+  loadPlayback = () => async () => {
+    const playing = await this.player.getPlaybackState();
 
-  // methods
+    if (playing?.item) {
+      this.track = playing.item as unknown as Track;
+    }
 
-  authenticate = async (accessToken: string) => {
-    try {
-      this.api.setAccessToken(accessToken);
-
-      await this.api.getMe();
-
-      this.statechart.send('authenticate', { accessToken });
-
-      return true;
-    } catch {
-      this.api.setAccessToken(null);
-
-      return false;
+    if (playing?.is_playing) {
+      this.statechart.send('play');
     }
   };
 
   play = async (options?: SpotifyApi.PlayParameterObject) => {
     await this.api.play({
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      device_id: this.deviceId,
       ...options
     });
   };
@@ -140,9 +191,11 @@ export class SpotifyClient {
     }
   };
 
+  // MISC
+
   setVolume = async (volumePercent: number) => {
     try {
-      await this.player.setVolume(volumePercent);
+      await this.api.setVolume(volumePercent);
     } catch {
       // not empty
     }
--- web/app/domain/supporting/spotify/components/player.gts
+++ web/app/domain/supporting/spotify/components/player.gts
@@ -1,14 +1,18 @@
 import Component from '@glimmer/component';
+import { fn } from '@ember/helper';
 
+import { command } from 'ember-command';
 import { service } from 'ember-polaris-service';
 import { and, not } from 'ember-truth-helpers';
 
-import { Icon } from '@hokulea/ember';
+import { Button, Icon, IconButton, Popover, popover } from '@hokulea/ember';
 
-import { formatArtists } from '../helpers';
+import { device2Icon, formatArtists } from '../helpers';
 import { SpotifyService } from '../service';
 import { LoginWithSpotify } from './login-with-spotify';
 import styles from './player.css';
+
+import type { Device } from '../domain-objects';
 
 export class SpotifyPlayer extends Component {
   @service(SpotifyService) declare spotify: SpotifyService;
@@ -17,37 +21,111 @@ export class SpotifyPlayer extends Component {
     return this.spotify.client;
   }
 
-  get player() {
-    return this.client.player;
-  }
-
   get track() {
     return this.client.track;
   }
 
+  get otherDevices() {
+    return this.client.devices.filter((dev) => dev.id !== this.client.device?.id);
+  }
+
+  selectDevice = async (device: Device) => {
+    await this.client.selectDevice(device);
+  };
+
   <template>
     <div class={{styles.layout}}>
-      <Icon @icon="spotify-logo" @style="fill" class={{styles.spotify}} />
+      <div class={{styles.content}}>
+        <Icon @icon="spotify-logo" @style="fill" class={{styles.spotify}} />
 
-      {{#if (and this.client.authenticated (not this.client.error))}}
-        {{#if this.client.ready}}
-          <p>
-            {{#if this.track}}
-              <strong>{{this.track.name}}</strong><br />
-              <small>{{formatArtists this.track.artists}}</small>
-            {{/if}}
-          </p>
-          {{!-- {{else if this.client.error}}
-          <p>
-            <Icon @icon="warning" @style="fill" class={{styles.warning}} />
-            Fehler:
-            {{this.client.error}}
-          </p> --}}
+        {{#if (and this.client.authenticated (not this.client.error))}}
+          {{!-- {{#if this.client.ready}}
+            <p>
+              {{#if this.track}}
+                <strong>{{this.track.name}}</strong><br />
+                <small>{{formatArtists this.track.artists}}</small>
+              {{/if}}
+            </p>
+            {{!-- {{else if this.client.error}}
+            <p>
+              <Icon @icon="warning" @style="fill" class={{styles.warning}} />
+              Fehler:
+              {{this.client.error}}
+            </p> --}
+          {{/if}} --}}
+
+          {{#if this.client.device}}
+            <p>
+              {{#if this.track}}
+                <strong>{{this.track.name}}</strong><br />
+                <small>{{formatArtists this.track.artists}}</small>
+              {{/if}}
+            </p>
+            {{!-- {{else if this.client.error}}
+            <p>
+              <Icon @icon="warning" @style="fill" class={{styles.warning}} />
+              Fehler:
+              {{this.client.error}}
+            </p> --}}
+          {{else}}
+            <p>
+              <Icon @icon="warning" @style="fill" class={{styles.warning}} />
+              Bitte Player auswählen
+              <Icon @icon="arrow-right" @style="thin" />
+            </p>
+          {{/if}}
+        {{else}}
+          <div><LoginWithSpotify /></div>
         {{/if}}
+      </div>
 
-      {{else}}
-        <div><LoginWithSpotify /></div>
-      {{/if}}
+      <div>
+        {{#if this.client.authenticated}}
+          {{#let (popover opened=this.client.loadDevices.perform) as |pop|}}
+            <IconButton
+              @icon="devices"
+              @iconStyle="bold"
+              @importance="plain"
+              @label="Spotify Player auswählen"
+              class={{styles.trigger}}
+              data-has-device={{if this.client.device true}}
+              data-need-device={{not this.client.device}}
+              {{pop.trigger}}
+            />
+
+            <Popover {{pop.target}} class={{styles.devices}}>
+              <b>Spotify Player</b>
+
+              {{#if this.client.device}}
+                <div class={{styles.device}}>
+                  <p>Aktueller Player</p>
+
+                  <p>
+                    <Icon @icon={{device2Icon this.client.device.type}} />
+                    {{this.client.device.name}}
+                  </p>
+                </div>
+              {{/if}}
+
+              {{#if this.otherDevices}}
+                <div>
+                  <p>Anderen Player wählen</p>
+
+                  {{#each this.otherDevices as |device|}}
+                    <Button
+                      @push={{command (fn this.selectDevice device) pop.close}}
+                      @importance="plain"
+                    >
+                      <:before><Icon @icon={{device2Icon device.type}} /></:before>
+                      <:label>{{device.name}}</:label>
+                    </Button>
+                  {{/each}}
+                </div>
+              {{/if}}
+            </Popover>
+          {{/let}}
+        {{/if}}
+      </div>
     </div>
   </template>
 }
