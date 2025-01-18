04e6f4ad9615be57f39360c29ee3281314bb3bf9
gossi <gossi@users.noreply.github.com>
2025-01-18 17:47:41 +0100

    Upgrade Ember

--- web/tests/test-helper.js
+++ web/tests/test-helper.js
@@ -1,16 +1,17 @@
 import { setApplication } from '@ember/test-helpers';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import { setupEmberOnerrorValidation, start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
 
 import Application from '@unidancing/app/app';
 import config from '@unidancing/app/config/environment';
 import setupSinon from 'ember-sinon-qunit';
 
 setApplication(Application.create(config.APP));
-
 setupSinon();
-
 setup(QUnit.assert);
+setupEmberOnerrorValidation();
 
+loadTests();
 start();
