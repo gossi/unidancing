b04e75f1e6ecc0c6c9f8fed34f7ed3678e891f8a
Thomas Gossmann <gossi@users.noreply.github.com>
2025-01-26 15:22:36 +0100

    using hono (#69)

--- gatekeeper/src/index.ts
+++ gatekeeper/src/index.ts
@@ -1,17 +1,10 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { sentry } from '@hono/sentry';
+import { cors } from 'hono/cors';
+import { Hono } from 'hono/tiny';
 
-import { Router } from 'itty-router';
 import { OauthClient } from './oauth-client';
-import { withSentry } from '@sentry/cloudflare';
 
+/* eslint-disable @typescript-eslint/naming-convention */
 export interface Env {
   ENVIRONMENT: 'dev' | 'staging' | 'production';
   SPOTIFY_CLIENT_ID: string;
@@ -20,8 +13,10 @@ export interface Env {
   APP_ROOT: string;
   SENTRY_DSN: string;
 }
+/* eslint-enable @typescript-eslint/naming-convention */
 
-let spotifyClient: OauthClient;
+let spotifyClient: OauthClient | undefined = undefined;
+
 async function getSpotifyClient(env: Env): Promise<OauthClient> {
   if (!spotifyClient) {
     spotifyClient = new OauthClient({
@@ -45,117 +40,56 @@ async function getSpotifyClient(env: Env): Promise<OauthClient> {
   return spotifyClient;
 }
 
-const router = Router();
+/* eslint-disable @typescript-eslint/naming-convention */
+const app = new Hono<{
+  Bindings: Env;
+  Variables: {
+    spotify: OauthClient;
+  };
+}>();
+/* eslint-enable @typescript-eslint/naming-convention */
 
-router.get('/spotify/login', async (_req, env: Env) => {
-  const spotify = await getSpotifyClient(env);
-  const loginUrl = await spotify.getAuthorizationURL();
-  return Response.redirect(loginUrl.toString());
+app.use(cors());
+app.use((c, next) => sentry({ environment: c.env.ENVIRONMENT })(c, next));
+
+app.use('/spotify/*', async (c, next) => {
+  c.set('spotify', await getSpotifyClient(c.env));
+
+  await next();
 });
 
-router.get('/spotify/authenticated', async (req, env: Env) => {
-  const spotify = await getSpotifyClient(env);
-  const result = await spotify.grantCode(req.url);
+app.get('/spotify/login', async (c) => {
+  const spotify = c.get('spotify');
+  const loginUrl = await spotify.getAuthorizationURL();
 
-  const redirectUrl = new URL(`${env.APP_ROOT}/auth/spotify`);
+  return c.redirect(loginUrl.toString());
+});
+
+app.get('/spotify/authenticated', async (c) => {
+  const spotify = c.get('spotify');
+  const result = await spotify.grantCode(c.req.url);
+  const redirectUrl = new URL(`${c.env.APP_ROOT}/auth/spotify`);
+
   redirectUrl.searchParams.set('access_token', result.access_token);
   redirectUrl.searchParams.set('refresh_token', result.refresh_token as string);
-  redirectUrl.searchParams.set('expires_in', `${result.expires_in}`);
+  redirectUrl.searchParams.set('expires_in', result.expires_in?.toString() as string);
 
-  return Response.redirect(redirectUrl.toString());
+  return c.redirect(redirectUrl.toString());
 });
 
-router.get('/spotify/refresh', async (req, env) => {
-  const url = new URL(req.url);
-  if (!url.searchParams.has('token')) {
-    return new Response('', {
-      status: 400,
-      statusText: 'Missing token query parameter'
-    });
+app.get('/spotify/refresh', async (c) => {
+  if (!c.req.query('token')) {
+    return c.notFound();
   }
 
-  const spotify = await getSpotifyClient(env);
-  const result = await spotify.refresh(url.searchParams.get('token') as string);
+  const spotify = c.get('spotify');
+  const result = await spotify.refresh(c.req.query('token') as string);
 
-  return new Response(JSON.stringify(result), {
-    status: 200,
-    headers: {
-      'content-type': 'application/json;charset=UTF-8'
-    }
-  });
+  return c.json(result);
 });
 
-// Reference: https://developers.cloudflare.com/workers/examples/cors-header-proxy
-const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
-  'Access-Control-Max-Age': '86400'
-};
+app.get('/fail', () => {
+  throw new Error('boom');
+});
 
-function handleOptions(request: Request) {
-  // Make sure the necessary headers are present
-  // for this to be a valid pre-flight request
-  const headers = request.headers;
-  if (
-    headers.get('Origin') !== null &&
-    headers.get('Access-Control-Request-Method') !== null &&
-    headers.get('Access-Control-Request-Headers') !== null
-  ) {
-    // Handle CORS pre-flight request.
-    // If you want to check or reject the requested method + headers
-    // you can do that here.
-    const respHeaders = {
-      ...corsHeaders,
-      // Allow all future content Request headers to go back to browser
-      // such as Authorization (Bearer) or X-Client-Name-Version
-      'Access-Control-Allow-Headers': request.headers.get(
-        'Access-Control-Request-Headers'
-      )
-    };
-    return new Response(null, {
-      // @ts-ignore
-      headers: respHeaders
-    });
-  } else {
-    // Handle standard OPTIONS request.
-    // If you want to allow other HTTP Methods, you can do that here.
-    return new Response(null, {
-      headers: {
-        Allow: 'GET, HEAD, POST, OPTIONS'
-      }
-    });
-  }
-}
-
-export default withSentry(
-  env => ({
-    environment: env.ENVIRONMENT,
-    dsn: env.SENTRY_DSN,
-    // Set tracesSampleRate to 1.0 to capture 100% of spans for tracing.
-    tracesSampleRate: 1.0,
-  }), {
-    async fetch(
-      request: Request,
-      env: Env
-      // ctx: ExecutionContext
-    ): Promise<Response> {
-      let response;
-      if (request.method === 'OPTIONS') {
-        response = handleOptions(request);
-      } else {
-        response = await router.fetch(request, env);
-
-        if (!response.redirected) {
-          response = new Response(response.body, response);
-          response.headers.set('Access-Control-Allow-Origin', '*');
-          response.headers.set(
-            'Access-Control-Allow-Methods',
-            'GET, POST, PUT, DELETE, OPTIONS'
-          );
-        }
-      }
-
-      return response;
-    }
-  }
-);
+export default app;
